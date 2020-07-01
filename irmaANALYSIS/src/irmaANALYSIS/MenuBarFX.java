8cddac12b7517e74dbd362141ee9f780419d45ee
Andreas Pirchner <ich@andreaspirchner.at>
2020-07-01 09:15:58 +0100

    update

--- irmaANALYSIS/src/irmaANALYSIS/MenuBarFX.java
+++ irmaANALYSIS/src/irmaANALYSIS/MenuBarFX.java
@@ -3,21 +3,25 @@ package irmaANALYSIS;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.VBox;
 
 public class MenuBarFX {
 	
 	MenuBar mb;
 	Menu fileMenu, controlMenu, analyzeMenu;
-	MenuItem f1, f2, c1, c2, c3, a1, a2, a3, a4; 
+	MenuItem f1, f2, f3, f4, c1, c2, c3, a1, a2, a3, a4; 
 	VBox vb;
     
 	MenuBarFX(){
 		MenuBar mb = new MenuBar();
-		fileMenu = new Menu("Menu"); 
+		fileMenu = new Menu("Project"); 
 		controlMenu = new Menu("Control");
 	    analyzeMenu = new Menu("Analyze");
 		
@@ -25,8 +29,10 @@ public class MenuBarFX {
 	    //m2 = new MenuItem("menu item 2"); 
 	    //m3 = new MenuItem("menu item 3"); 
 	    
-	    f1=new MenuItem("Load Sample");  
-	     f2=new MenuItem("Quit");  
+	     f1=new MenuItem("Load Sample Data");  
+	     f2=new MenuItem("Load Project");
+	     f3=new MenuItem("Save Project");
+	     f4=new MenuItem("Quit");
 	     c1=new MenuItem("Play");  
 	     c2=new MenuItem("Pause");  
 	     c3=new MenuItem("Stop");  
@@ -34,19 +40,28 @@ public class MenuBarFX {
 	     a2=new MenuItem("Sample – Activity ");  
 	     a3=new MenuItem("Subjects – Focus");  
 	     a4=new MenuItem("Subjects – Activity");
+	     
+	     f1.setAccelerator(KeyCombination.keyCombination("Ctrl+L"));
+	     f4.setAccelerator(KeyCombination.keyCombination("Ctrl+Q"));
+	     f1.setOnAction(new EventHandler<ActionEvent>() {
+	         public void handle(ActionEvent event) {
+	           System.out.println("Load Sample Data");
+	           
+	         }
+	       });
+	     
+	     f4.setOnAction(new EventHandler<ActionEvent>() {
+	         public void handle(ActionEvent event) {
+	           System.out.println("Quit");
+	           System.exit(0);
+	         }
+	       });
 	    
-	     fileMenu.getItems().add(f1); 
-	     fileMenu.getItems().add(f2); 
-	     controlMenu.getItems().add(c1); 
-	     controlMenu.getItems().add(c2);
-	     controlMenu.getItems().add(c3);
-	     analyzeMenu.getItems().add(a1);
-	     analyzeMenu.getItems().add(a2);
-	     analyzeMenu.getItems().add(a3);
-	     analyzeMenu.getItems().add(a4);
-	    mb.getMenus().add(fileMenu); 
-	    mb.getMenus().add(controlMenu); 
-	    mb.getMenus().add(analyzeMenu); 
+	     fileMenu.getItems().addAll(f1, new SeparatorMenuItem(), f2, f3, new SeparatorMenuItem(), f4);
+	     controlMenu.getItems().addAll(c1, c2, c3);
+	     analyzeMenu.getItems().addAll(a1, a2, new SeparatorMenuItem(), a3, a4);
+
+	    mb.getMenus().addAll(fileMenu, controlMenu, analyzeMenu);
 	    vb = new VBox(mb);
 	}
 	
